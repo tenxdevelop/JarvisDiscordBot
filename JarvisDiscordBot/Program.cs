abda2f5e3dde0b0ad6229d19ba73a56870432081
tenxdeveloper <tenxdeveloper@bk.ru>
2024-11-14 22:45:33 +0500

    Create project

---
+++ JarvisDiscordBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+
+namespace JarvisDiscordBot
+{
+    internal class Program
+    {
+        private static DiscordClient m_jarvisClient { get; set; }
+        private static CommandsNextExtension m_commandsExtension { get; set; }
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}
