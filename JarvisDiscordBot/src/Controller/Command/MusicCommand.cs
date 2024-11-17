91be990fe60c0631066f3da6c3ad6beaec2786ca
tenxdeveloper <tenxdeveloper@bk.ru>
2024-11-17 17:46:05 +0500

    Include lavalink

---
+++ JarvisDiscordBot/src/Controller/Command/MusicCommand.cs
@@ -0,0 +1,27 @@
+﻿/**************************************************************************\
+    Copyright SkyForge Corporation. All Rights Reserved.
+\**************************************************************************/
+
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.CommandsNext;
+using JarvisDiscordBot.Models;
+using System.Threading.Tasks;
+
+namespace JarvisDiscordBot.Controller
+{
+    public sealed class MusicCommand : BaseCommandModule, IDiscordCommandModel
+    {
+        [Command("play")]
+        public async Task PlayMusic(CommandContext commandContext, string query)
+        {
+            var userVoiceChanel = commandContext.Member.VoiceState.Channel;
+
+            if (commandContext.Member.VoiceState is null || userVoiceChanel is null)
+            {
+                await commandContext.Channel.SendMessageAsync("Please enter a any chanel");
+                return;
+            }
+
+        }
+    }
+}
